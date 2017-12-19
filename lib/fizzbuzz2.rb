6deab2dfab16cf75e4e6618bcb62c510473bb8b0
Holger_Tidemand <tidemand.holger@gmail.com>
2017-12-19 08:35:45 +0100

    added new solution with case statement

---
+++ lib/fizzbuzz2.rb
@@ -0,0 +1,11 @@
+
+
+def fizz_buzz(number)
+  case
+  when (number.is_a? String) || (number < 0) then 'Error 404, Please insert a number above Zero!'
+  when (number % 3 == 0) && (number % 5 == 0) then 'fizz_buzz'
+  when number % 5 == 0 then 'buzz'
+  when number % 3 == 0 then 'fizz'
+  else number
+  end
+end
--- spec/fizz_buss_spec.rb
+++ spec/fizz_buss_spec.rb
@@ -1,4 +1,4 @@
-require './lib/fizz_buzz'
+require './lib/fizzbuzz2'
 
 describe 'fizz_buzz' do
 
@@ -6,19 +6,19 @@ it 'returns 1 if number is 1' do
   expect(fizz_buzz(1)).to eq 1
 end
 
-it 'returns fizz if number is divisible by 3' do
+ it 'returns fizz if number is divisible by 3' do
   expect(fizz_buzz(3)).to eq 'fizz'
-end
-
+ end
+#
 it 'returns buzz if number is divisiable by 5' do
   expect(fizz_buzz(5)).to eq 'buzz'
 end
-
-it 'Returns fizz_buzz if number is divisable by 15' do
-  expect(fizz_buzz(15)).to eq 'fizz_buzz'
-end
-
-it 'returns a error message if not a Integer' do
-  expect(fizz_buzz(String)).to eq 'Error 404, Please insert a number'
-end
+#
+ it 'Returns fizz_buzz if number is divisable by 15' do
+   expect(fizz_buzz(15)).to eq 'fizz_buzz'
+ end
+#
+ it 'returns a error message if not a Integer' do
+  expect(fizz_buzz("Holger")).to eq 'Error 404, Please insert a number above Zero!'
+ end
 end
